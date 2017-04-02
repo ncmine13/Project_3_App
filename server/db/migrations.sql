d633a1320a087425b8b49480a438ddb7fd325d2a
Naomi Mine <naomi.mine@gmail.com>
2017-04-02 14:03:19 -0500

    second

---
+++ server/db/migrations.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE moodtracker
+
+\c moodtracker
+
+CREATE TABLE users (id SERIAL PRIMARY KEY, username varchar(255), password_digest varchar(255))
+
+CREATE TABLE moods (id SERIAL PRIMARY KEY, word1 varchar(255), word2 varchar(255), word3 varchar(255), worst varchar(255), best varchar(255), worry varchar(255), confidence INT, satisfaction INT, stress INT, sadness INT, anger INT, happiness INT, funny INT, thing1 varchar(255), thing2 varchar(255), thing3 varchar(255))
