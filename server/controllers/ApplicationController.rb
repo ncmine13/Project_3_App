f53e828c5aed098d0ef05b09e01b88f5beb225c6
Naomi Mine <naomi.mine@gmail.com>
2017-05-04 11:20:27 -0500

    s

--- server/controllers/ApplicationController.rb
+++ server/controllers/ApplicationController.rb
@@ -4,24 +4,24 @@ class ApplicationController < Sinatra::Base
 	require 'bundler'
 	Bundler.require
 	
-	# db = URI.parse(ENV['DATABASE_URL'])
+	db = URI.parse(ENV['DATABASE_URL'])
 	
- # 	DB_NAME = db.path[1..-1]
+ 	DB_NAME = db.path[1..-1]
 	
-	# 	ActiveRecord::Base.establish_connection(
-	# 	:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-	# 	:host     => db.host,
-	# 	:port     => db.port,
-	# 	:username => db.user,
-	# 	:password => db.password,
-	# 	:database => DB_NAME,
-	# 	:encoding => 'utf8'
-	# 	)
+		ActiveRecord::Base.establish_connection(
+		:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+		:host     => db.host,
+		:port     => db.port,
+		:username => db.user,
+		:password => db.password,
+		:database => DB_NAME,
+		:encoding => 'utf8'
+		)
 
-	ActiveRecord::Base.establish_connection(
-		:adapter => 'postgresql',
-		:database => 'moodtracker'
-	)
+	# ActiveRecord::Base.establish_connection(
+	# 	:adapter => 'postgresql',
+	# 	:database => 'moodtracker'
+	# )
 
 	set :views, File.expand_path('../../views', __FILE__)
 	set :public_dir, File.expand_path('../../public', __FILE__)
